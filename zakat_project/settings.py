6fd656ad42e7129eee99db735616e9e9862bf7ac
hanifubaaba <baabahanif88@gmail.com>
2025-03-07 06:17:26 +0100

    debug

--- zakat_project/settings.py
+++ zakat_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
 DEBUG = True
-DEBUG = os.getenv("DEBUG", "False") == "True"
+# DEBUG = os.getenv("DEBUG", "False") == "True"
 
 
 
